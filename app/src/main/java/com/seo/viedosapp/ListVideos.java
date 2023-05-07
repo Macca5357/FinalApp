6c11e053e3d78d1f725ca730dd474d8e8546745b
Macca5357 <19237766@studentmail.ul.ie>
2023-05-07 23:21:45 +0100

    Database

---
+++ app/src/main/java/com/seo/viedosapp/ListVideos.java
@@ -0,0 +1,14 @@
+/* Student name: Gavin McCarthy
+ * Student id: 19237766
+ */
+package com.seo.viedosapp;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class ListVideos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_videos);
+    }
+}
